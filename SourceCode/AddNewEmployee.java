876da543c10888aae2e720f1fb3e6a149394aa43
Mudreka Arif <mudrekaarif@gmail.com>
2020-05-09 18:33:57 +0530

    Feature01 AddingNewEmployee 	Modified File : AddNewEmployee.java 	Solved : Take input from user and enter into FKpayroll Database,into table Employee. 	Todo : Solve Input errors.

--- SourceCode/AddNewEmployee.java
+++ SourceCode/AddNewEmployee.java
@@ -1,5 +1,6 @@
 package payrollDesign;
 import java.util.Scanner;
+import java.sql.*;  
 
 public class AddNewEmployee {
 	public static void main(String args[]) {
@@ -11,6 +12,23 @@ public class AddNewEmployee {
 		if("y".equals(confirm)) {
 			System.out.println("Your Flipkart ID is : ");
 			System.out.println(emp.GetID());
+			try{  
+				Class.forName("com.mysql.cj.jdbc.Driver");  
+				Connection con=DriverManager.getConnection(  
+				"jdbc:mysql://localhost:3306/FKpayroll","root","");
+				Statement stmt=con.createStatement();
+				String temp = "INSERT INTO Employee VALUES " + "('" + 
+				emp.GetName() + "','" + emp.GetID() + "','" + emp.GetContactNumber() + 
+				"'," + emp.GetEmployeeType() + "," + emp.GetUnionCharge() + "," +
+				emp.GetSalary() + "," + emp.GetSalaryPHr() + "," + emp.GetComissionRate() + 
+				"," + emp.GetPaymentType() + ")";
+				System.out.println(temp);
+				stmt.executeUpdate(temp);
+				con.close();
+			}
+			catch(Exception e){ 
+				System.out.println(e);
+			}
 		}
 	}
 }
@@ -21,11 +39,16 @@ class Employee {
 	private String contactNumber;
 	private int employeeType = 0;
 	private int paymentType = 0;
+	private double salary = 0;
+	private double salaryPHr = 0;
+	private double comissionRate = 0;
+	private double unionCharge = 0;
 	Employee() {
 		EnterName();
 		Details();
 		EmployeeAndPaymentType();
 		GenerateCID();
+		SalaryDetails();
 	}
 	Scanner in = new Scanner(System.in);
 	private void EnterName() {
@@ -44,9 +67,8 @@ class Employee {
 		System.out.println("Choose your Employee Type:");
 		System.out.println("1. Paid by hour");
 		System.out.println("2. Paid a flat salary");
-		System.out.println("3. Fixed salary + commision");
 		this.employeeType = in.nextInt();
-		while(this.employeeType != 1 && this.employeeType != 2 && this.employeeType != 3) {
+		while(this.employeeType != 1 && this.employeeType != 2) {
 			System.out.println("Invalid Choice, Choose again.");
 			this.employeeType = in.nextInt();
 		}
@@ -63,6 +85,20 @@ class Employee {
 		}
 		System.out.println();
 	}
+	private void SalaryDetails() {
+		if(this.employeeType == 1) {
+			System.out.println("Enter per hour salary :");
+			this.salaryPHr = in.nextDouble();
+		}
+		else {
+			System.out.println("Enter monthly salary :");
+			this.salary = in.nextDouble();
+		}
+		System.out.println("Enter Comission rate :");
+		this.comissionRate = in.nextDouble();
+		System.out.println("Enter Union Charge :");
+		this.unionCharge = in.nextDouble();
+	}
 	private void GenerateCID() {
 		this.companyID = this.firstName + "." + this.lastName + "." + this.contactNumber + "@flipkart.com";
 	}
@@ -72,4 +108,26 @@ class Employee {
 	public String GetName() {
 		return this.firstName + " " + this.lastName;
 	}
+	public String GetContactNumber() {
+		return this.contactNumber;
+	}
+	public double GetSalary() {
+		return this.salary;
+	}
+	public double GetSalaryPHr() {
+		return this.salaryPHr;
+	}
+	public double GetComissionRate() {
+		return this.comissionRate;
+	}
+	public double GetUnionCharge() {
+		return this.unionCharge;
+	}
+	public int GetPaymentType() {
+		return this.paymentType;
+	}
+	public int GetEmployeeType() {
+		return this.employeeType;
+	}
+
 }
